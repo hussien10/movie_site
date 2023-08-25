aa98a238c618744fe3ca2ca16eec3723152e4bc9
hussien10 <hussienmo385@gmail.com>
2023-08-25 15:06:24 +0300

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAlUBefW36PlQHXWJ15K_vZJu7-qSFq3TM",
+    authDomain: "noxe-c14e8.firebaseapp.com",
+    projectId: "noxe-c14e8",
+    storageBucket: "noxe-c14e8.appspot.com",
+    messagingSenderId: "646386904641",
+    appId: "1:646386904641:web:b895a703d8aecf0f4a60c0",
+    measurementId: "G-H9KCGBS4WQ"
+  }
 };
