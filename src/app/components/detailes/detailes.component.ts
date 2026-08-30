a049bd11e171111b83a6bc49b62d03bec8d7289a
hussien10 <32900347+hussien10@users.noreply.github.com>
2026-08-30 05:17:16 +0300

    Set dynamic per-movie/show/person title, description and OG image on details page

--- src/app/components/detailes/detailes.component.ts
+++ src/app/components/detailes/detailes.component.ts
@@ -2,6 +2,7 @@ import { MoviesService } from './../../services/movies.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SeoService } from '../../services/seo.service';
 // import { Route } from '@angular/compiler/src/core';
 @Component({
   selector: 'app-detailes',
@@ -9,7 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./detailes.component.scss']
 })
 export class DetailesComponent implements OnInit,OnDestroy {
-   constructor(private _moviesService:MoviesService,private _ActivatedRoute:ActivatedRoute,private _router:Router) {
+   constructor(private _moviesService:MoviesService,private _ActivatedRoute:ActivatedRoute,private _router:Router,private _seoService:SeoService) {
     this._router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
   imagePaseUrl:string=`https://image.tmdb.org/t/p/w500`
@@ -29,6 +30,16 @@ export class DetailesComponent implements OnInit,OnDestroy {
     this.movieSub=this._moviesService.getDetailes(this.cat,this.id).subscribe(response=>{
       this.movieDetailes=response
       this.vote=Math.floor(this.movieDetailes.vote_average/2)
+
+      const displayName = this.movieDetailes?.original_name || this.movieDetailes?.original_title || this.movieDetailes?.name;
+      const description = (this.movieDetailes?.overview || this.movieDetailes?.biography || '').slice(0, 160);
+      const image = this.movieDetailes?.poster_path || this.movieDetailes?.profile_path;
+      this._seoService.update({
+        title: displayName,
+        description: description || `View details for ${displayName} on Noxe.`,
+        image: image ? `${this.imagePaseUrl}${image}` : undefined,
+        url: `/detailes/${this.cat}/${this.id}`
+      })
 
     })
   }
