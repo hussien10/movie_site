b1648366056dcb5e4415c717a2a0d27493b0f258
hussien10 <32900347+hussien10@users.noreply.github.com>
2026-08-30 05:16:49 +0300

    Wire SeoService to update title/meta tags on route navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { SeoService } from './services/seo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'noxe';
+
+  constructor(
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private _seoService: SeoService
+  ) { }
+
+  ngOnInit(): void {
+    this._router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let route = this._activatedRoute;
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      mergeMap(route => route.data)
+    ).subscribe(data => {
+      if (data && data['title']) {
+        this._seoService.update({
+          title: data['title'],
+          description: data['description'],
+          url: data['url']
+        });
+      }
+    });
+  }
 }
